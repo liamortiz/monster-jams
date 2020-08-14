a52a1d053ff2fe1ea3df70b9607de9173e125976
Leamsi Escribano <leamsi.esc@gmail.com>
2020-08-13 21:25:53 -0400

    Fixing audio player

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,36 +1,70 @@
 import React from 'react';
-import MusicNode from './MusicNode';
 import soundData from '../sounds.json';
-import { MetalSynth } from "tone";
-
+import { Synth } from "tone";
 
 class Playlist extends React.Component {
 
     constructor(props) {
         super(props);
-        //this.props.genre
         this.sounds = soundData["tropical"]
-        this.synth = new MetalSynth().toDestination();
-    }
+        this.synth = new Synth().toDestination();
+        this.musicNodes = [];
+        this.activeNodes = [];
 
-    createGrid() {
-        const grid = [];
+        this.createMusicNodes();
+        this.getMusicNodes();
+
+        this.play = this.play.bind(this);
+    }
+    createMusicNodes() {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 6; j++) {
-                grid.push(<MusicNode handleClick = { this.handleClick } sound = {this.sounds[i]}/>)
+                if (!this.musicNodes[i]) {
+                    this.musicNodes[i] = [];
+                }
+                this.musicNodes[i].push({active: false, sound: this.sounds[i], pos: j})
             }
         }
-        return grid;
     }
 
-    handleClick = (sound) => {
-        this.synth.triggerAttackRelease(sound, "100n");
+    getMusicNodes() {
+        let nodes = []
+        for (const nodeArr of this.musicNodes) {
+            for (const node of nodeArr) {
+                nodes.push(<div className = {`node ${node.active ? " active" : ""}`} onClick={() => {this.handleClick(node)}} ></div>)
+            }
+        }
+        return nodes;
     }
+
+    handleClick = (node) => {
+        node.active = !node.active
+        if (node.active) {
+            this.synth.triggerAttackRelease(node.sound, "8n");
+        }
+
+        this.forceUpdate();
+    }
+
+    play() {
+        for (const nodeArr of this.musicNodes) {
+            for (const node of nodeArr) {
+                if (node.active) {
+                    //console.log(node.pos);
+                    //this.synth.triggerAttackRelease(node.sound, "8n", node.pos * 2); This explodes.
+                }
+            }
+        }
+    }
+
     render() {
         return (
-            <div id = "playlist">
-                {this.createGrid()}
-            </div>
+            <React.Fragment>
+                <button onClick = {this.play}>Play</button>
+                <div id = "playlist">
+                {this.getMusicNodes()}
+                </div>
+            </React.Fragment>
         )
     }
 }
